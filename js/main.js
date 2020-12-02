dae24dc2a30b67c3e0a5a89c319668d1e8d0b8c4
Shnaeem <62766356+Shnaeem@users.noreply.github.com>
2020-12-01 19:31:04 -0500

    Tuesday commit 1

---
+++ js/main.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { async } from 'q';
+dotenv.config();
+
+const BASE_URL = process.env.BASE_URL;
+const getDataButton = document.getElementById('getTemp')
+
+getDataButton.addEventListener("click",(e)=>{
+    e.preventDefault();
+    let url = `${BASE_URL}quotes/random`
+    //https://quote-garden.herokuapp.com/api/v2/quotes/random
+
+
+
+    axios.get(url)
+        .then( (response) =>{
+       //console.log(response);
+            let tem = document.querySelector('.tem');
+            tem.innerHTML= `${response.data.quote.quoteText} \n
+            ${response.data.quote.quoteAuthor}
+            ${response.data.quote.quoteGenre}`
+        })
+        .catch( (error)=> {
+            // handle error
+            let tem = document.querySelector('.tem');
+            tem.innerHTML= `Please enter the keyword again`
+            console.log(error);
+        })
+
+
+        })
